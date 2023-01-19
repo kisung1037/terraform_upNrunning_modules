0a5f5dc14679b7b45c583d95ed24b452842f9693
KeyS <kisung1037@gmail.com>
2023-01-19 17:25:42 +0900

    .

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -86,6 +86,29 @@ resource "aws_autoscaling_group" "test" {
     }
 }
 
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+    count = var.enable_autoscaling? 1 : 0
+
+    scheduled_action_name = "${var.cluster_name}-scale-out-during-business-hours"
+    min_size = 2
+    max_size = 10
+    desired_capacity = 10
+    recurrence = "0 9 * * *"
+    autoscaling_group_name = aws_autoscaling_group.test.name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+    count = var.enable_autoscaling? 1 : 0
+
+    scheduled_action_name = "${var.cluster_name}-scale_in_at_night"
+    min_size = 2
+    max_size = 10
+    desired_capacity = 2
+    recurrence = "0 17 * * *"
+    autoscaling_group_name = aws_autoscaling_group.test.name
+}
+
+
 
 data "aws_vpc" "default" {
     default = true
